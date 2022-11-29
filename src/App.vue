cf69304449b5a3e5ab7c9339fcc3c6765fff3efb
Olivier Verwoerd <olivierverwoerd@gmail.com>
2022-11-29 16:23:54 +0100

    router + config test

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,31 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg?url" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg?url" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="header">
+    <h1> (</h1>
+    <h2>//</h2>
+    <h1> O W O </h1>
+    <h2>//</h2>
+    <h1>)</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <router-view></router-view>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style scoped lang="scss">
+.header {
+  display: flex;
+  justify-content: center;
+
+  h1 {
+    font-size: 50px;
+  }
+
+  h2 {
+    margin: 50px 20px 0 20px;
+    font-size: 50px;
+    color: pink;
+  }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import "./style.css";
+import App from "./App.vue";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
 
-createApp(App).mount('#app')
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{ name: "Home", path: "/", component: Home },
+		{ name: "About", path: "/about", component: About },
+	],
+});
+
+const app = createApp(App);
+// Make sure to _use_ the router instance to make the
+// whole app router-aware.
+app.use(router);
+
+app.mount("#app");
