8250a2f84ceae7f8bf78c3bb4e9052d458688f0b
NorthLake <martijn.noordermeer@gmail.com>
2023-03-21 17:57:26 +0100

    Carousel Over Ons

---
+++ src/layouts/Carousel.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+  import Glide from '@glidejs/glide';
+  import { onMounted } from 'vue';
+
+  onMounted(async () => {
+    const glide = new Glide('.glide', {
+      type: 'carousel',
+      autoplay: 5000,
+      animationDuration: 800,
+      swipeThreshold: false,
+      dragThreshold: false,
+      hoverpause: false,
+    });
+
+    glide.mount();
+  });
+
+  defineProps({
+    images: Array<string>
+  });
+
+
+</script>
+
+<template>
+  <div class="photos">
+    <div class="glide">
+      <div class="glide__track" data-glide-el="track">
+        <ul class="glide__slides">
+          <li class="glide__slide" v-for="image in images">
+            <img class="image" :src="image">
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @import "@glidejs/glide/src/assets/sass/glide.core";
+
+  * {
+    max-height: inherit;
+  }
+
+  .image {
+    object-fit: cover;
+  }
+
+  .photos {
+    max-width: 90vw;
+    margin: 0 auto;
+  }
+
+  .glide {
+    cursor: initial;
+  }
+</style>
--- src/views/OverIndicium.vue
+++ src/views/OverIndicium.vue
@@ -1,32 +1,41 @@
 <script setup lang="ts">
-import ContentContainer from '@/layouts/ContentContainer.vue';
+  import ContentContainer from '@/layouts/ContentContainer.vue';
+  import Carousel from '@/layouts/Carousel.vue';
+
+  const images = [
+    "/assets/images/borrel2019.JPG",
+    "/assets/images/sebTappen.JPG",
+    "/assets/images/fotoOff.JPG",
+  ];
 
 </script>
 
 <template>
     <ContentContainer>
+      <div class="carousel">
+        <Carousel :images="images" />
+      </div>
       <div>
         <h1>Over Indicium</h1>
-        <h2>Indicium is de studievereniging voor studenten van het Institute for ICT aan de Hogeschool Utrecht. De vereniging organiseert naast informele activiteiten zoals poolen, bowlen, karten en feestjes met andere verenigingen ook verdiepende activiteiten in de ICT branche zoals lezingen, workshops en inhousedagen. Daarnaast biedt Indicium ook netwerkmogelijkheden voor studenten, waar ze in contact kunnen komen met bedrijven en professionals uit de branche. De vereniging heeft ook een actieve rol in de organisatie van evenementen, zoals 2 maal per jaar een studiereis naar beurzen door Europa en hackathons. </h2>
-        <h1>Introkamp</h1>
-        <h2>Verreweg een van de leukste activiteiten van het jaar is het introkamp vlak voor het begin van het studiejaar. Tijdens het introkamp organiseren de leden van de vereniging activiteiten die gericht zijn op teambuilding en gezelligheid. Tijdens het introkamp maak je door verschillende activiteiten kennis met de stad Utrecht. Daarna verhuist de gezelligheid naar een evenementenlocatie in de buurt van de stad waar de gezelligheid verder gaat door middel van uitdagende activiteiten, teambuilding en een leuk feestje.
+        <p>Indicium is de studievereniging voor studenten van het Institute for ICT aan de Hogeschool Utrecht. De vereniging organiseert naast informele activiteiten zoals poolen, bowlen, karten en feestjes met andere verenigingen ook verdiepende activiteiten in de ICT branche zoals lezingen, workshops en inhousedagen. Daarnaast biedt Indicium ook netwerkmogelijkheden voor studenten, waar ze in contact kunnen komen met bedrijven en professionals uit de branche. De vereniging heeft ook een actieve rol in de organisatie van evenementen, zoals 2 maal per jaar een studiereis naar beurzen door Europa en hackathons. </p>
+        <h2>Introkamp</h2>
+        <p>Verreweg een van de leukste activiteiten van het jaar is het introkamp vlak voor het begin van het studiejaar. Tijdens het introkamp organiseren de leden van de vereniging activiteiten die gericht zijn op teambuilding en gezelligheid. Tijdens het introkamp maak je door verschillende activiteiten kennis met de stad Utrecht. Daarna verhuist de gezelligheid naar een evenementenlocatie in de buurt van de stad waar de gezelligheid verder gaat door middel van uitdagende activiteiten, teambuilding en een leuk feestje.
         Kortom, het introkamp is de beste manier om je studententijd in Utrecht te beginnen en vrienden te maken voor de rest van je studie.
-        </h2>
+        </p>
       </div>
     </ContentContainer>
 </template>
 
 <style scoped>
-div {
-  width: 1000px;
-  align-self: center;
-}
-h2 {
-    font-size: 25px;
-    text-align: center;
-}
+  .carousel {
+    margin: 0 auto;
+    margin-top: 2em;
+    max-height: 32em;  /* gewoon fotoshoppen, niet moeilijk doen */
+    max-width: 1200px;
+  }
 
-p {
-    text-align: center;
-}
+  p {
+    width: 70%;
+    margin: 0 auto;
+  }
 </style>
