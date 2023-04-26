d01a4df8a08c4ee7c2c365354350a6a2006c063b
Olivier Verwoerd <olivierverwoerd@gmail.com>
2023-04-26 15:40:31 +0200

    Css fixes and removed glider as it's broken

--- src/views/Activiteiten.vue
+++ src/views/Activiteiten.vue
@@ -1,21 +1,25 @@
 <script setup lang="ts">
-
-import ContentContainer from '@/layouts/ContentContainer.vue';
+import ContentContainer from "@/layouts/ContentContainer.vue";
 </script>
 
 <template>
-    <ContentContainer>
-        <h1>Agenda</h1>
-        <iframe
-            src="https://calendar.google.com/calendar/embed?src=c_cb2b2ab9761bec69a9d24fd452f2d970d31755cf1c382272560d81fddca0e5e5%40group.calendar.google.com&ctz=Europe%2FBrussels"
-            style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-    </ContentContainer>
+  <ContentContainer>
+    <h1>Agenda</h1>
+    <iframe
+      src="https://calendar.google.com/calendar/embed?src=c_cb2b2ab9761bec69a9d24fd452f2d970d31755cf1c382272560d81fddca0e5e5%40group.calendar.google.com&ctz=Europe%2FBrussels"
+      style="border: 0"
+      height="600"
+      frameborder="0"
+      scrolling="no"
+    ></iframe>
+  </ContentContainer>
 </template>
 
 <style scoped>
-
+iframe {
+  width: -webkit-fill-available;
+}
 </style>
-
 
 https://calendar.google.com/calendar/ical/c_cb2b2ab9761bec69a9d24fd452f2d970d31755cf1c382272560d81fddca0e5e5%40group.calendar.google.com/public/basic.ics
 https://calendar.google.com/calendar/ical/o963jj1htc0hjmmt6e3874gbq4%40group.calendar.google.com/public/basic.ics
