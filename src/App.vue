f3254d1661fab77c86592fc8b06b4a94cf6ab017
GreenMan36 <Jaspervandenbremer@gmail.com>
2023-05-25 10:49:59 +0200

    FIX: footer not staying a footer on big screens/low page contents

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import NavDesktop from "@/layouts/Nav.vue"
-import NavMobile from "@/layouts/NavMobile.vue"
-import FooterMenu from "@/layouts/Footer.vue"
+import NavDesktop from '@/layouts/Nav.vue';
+import NavMobile from '@/layouts/NavMobile.vue';
+import FooterMenu from '@/layouts/Footer.vue';
 
-import { stateStore } from "@/stores/state";
+import { stateStore } from '@/stores/state';
 
 const state = stateStore();
 
-state.refreshTheme()
+state.refreshTheme();
 </script>
 
 <template>
@@ -21,12 +21,12 @@ state.refreshTheme()
 
 <style lang="scss">
 @font-face {
-  font-family: "Indicium-font";
+  font-family: 'Indicium-font';
   font-weight: 400;
   font-style: normal;
   font-display: auto;
   unicode-range: U+000-5FF;
-  src: local("Constantia"), url("/fonts/constan.woff2") format("woff2");
+  src: local('Constantia'), url('/fonts/constan.woff2') format('woff2');
 }
 
 body {
@@ -70,12 +70,20 @@ body {
 
 #app {
   width: 100vw;
-  display: table;
+  width: 100dvw;
+  min-height: 100vh;
+  min-height: 100dvh;
+  display: flex;
+  flex-direction: column;
   margin: 0;
   padding: 0;
 
   font-family: var(--font-family);
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+
+  .page-content {
+    flex-grow: 1;
+  }
 }
 </style>
