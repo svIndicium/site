0be760325ace560f31e0817ed06d9353704778c9
GreenMan36 <Jaspervandenbremer@gmail.com>
2023-05-25 12:57:39 +0200

    WIP: center form, use different colors

--- src/views/LidWorden.vue
+++ src/views/LidWorden.vue
@@ -20,15 +20,57 @@ confetti({
 <template>
   <ContentContainer>
     <h1>Aanmelden lidmaatschap INDICIUM</h1>
-    <iframe src="https://leden.conscribo.nl/svIndicium/aanmeldenlidmaatschap"></iframe>
+    <div class="signupContainer">
+      <iframe src="https://leden.conscribo.nl/svIndicium/aanmeldenlidmaatschap"></iframe>
+    </div>
+    <div>
+      Gaat er iets niet helemaal goed? Geen zorgen,
+      <a href="https://leden.conscribo.nl/svIndicium/aanmeldenlidmaatschap" target="_blank"
+        >open het formulier in een nieuw tabblad</a
+      >
+      en probeer het nog een keer. 🙂
+    </div>
   </ContentContainer>
 </template>
 
 <style scoped lang="scss">
+h1 {
+  margin-bottom: 24px;
+}
+
+.content-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
 iframe {
-  border: 0px;
+  flex-grow: 1;
+  border: none;
   width: 100%;
-  height: 1000px;
-  background-color: var(--signup-background);
+  height: 100%;
+  filter: var(--filter);
+}
+
+.signupContainer {
+  background-color: var(--secondary-background-color);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 900px;
+  max-width: 1200px;
+  border-radius: 4px;
+  margin-bottom: 48px;
+}
+
+.newtab-button {
+  background-color: var(--indi-blue-1);
+  padding: 8px;
+  margin-bottom: 16px;
+  border-radius: 8px;
+  color: var(--text-color);
+  font-weight: 600;
 }
 </style>
