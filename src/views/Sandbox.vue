14fe9a84c57c7e7054ca23200aa87eac7395bc2b
Olivier Verwoerd <olivierverwoerd@gmail.com>
2023-06-07 16:34:21 +0200

    added new icons

--- src/views/Sandbox.vue
+++ src/views/Sandbox.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 /// <reference types="vite-svg-loader" />
+import PaySuccess from '@/assets/icons/successful-payment.svg?component';
 import ContentContainer from '@/layouts/ContentContainer.vue';
 </script>
 
 <template>
   <ContentContainer>
     <p>A sandbox to test new features</p>
+    <PaySuccess />
   </ContentContainer>
 </template>
 
