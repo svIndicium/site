a92b16898ef4b4c7f963d740be88f4f43c9f3413
GreenMan36 <Jaspervandenbremer@gmail.com>
2023-07-18 18:14:39 +0200

    UPDATE: /into page style

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,77 +1,145 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-
-// this reference line is needed for TS and svg loader. Copy and past where needed
-/// <reference types="vite-svg-loader" />
-import theeSVG from '@/assets/icons/indicium_418.svg?component';
 import ContentContainer from '@/layouts/ContentContainer.vue';
 </script>
 
 <template>
   <ContentContainer>
-    <img :src="'/assets/images/intro2022.jpeg'" alt="'Intro'" />
+    <img :src="'/public/assets/images/intro2022.jpeg'" alt="'Intro'" />
+
+    <h1>Introductiekamp 2023</h1>
 
     <section class="intro">
-      <p>
-        Vanuit de Studievereniging Indicium zijn we blij om te zien dat je de keuze hebt gemaakt voor de HBO-ICT
-        opleiding <em>(inclusief Open-ICT)</em> Om je een warm welkom te bieden en een startpunt voor nieuwe
-        vriendschappen, hebben we een speciaal introductiekamp in het leven geroepen.
-      </p>
+      <div>
+        <p>
+          Vanuit de Studievereniging Indicium zijn we blij om te zien dat je de keuze hebt gemaakt voor de HBO-ICT
+          opleiding <em class="openict">(inclusief Open-ICT 💖)</em>. Om je een warm welkom te bieden en een startpunt
+          voor nieuwe vriendschappen, hebben we een speciaal introductiekamp in het leven geroepen.
+        </p>
 
-      <p>
-        Dit introductiekamp zal plaatsvinden van dinsdag 22 augustus tot en met vrijdag 25 augustus. De locaties zijn de
-        prachtige stad Utrecht en een plek nabij Geldermalsen. Hier krijg je de kans om je medestudenten beter te leren
-        kennen en de unieke sfeer van onze vereniging te ervaren. Er wachten je diverse activiteiten, variërend van
-        spellen tot sportuitdagingen, en het geheel wordt afgesloten met een gezellig feest!
-      </p>
+        <p>
+          Dit introductiekamp zal plaatsvinden van <em class="datum">dinsdag 22 augustus</em> tot en met
+          <em class="datum">vrijdag 25 augustus</em>. De locaties zijn de prachtige stad Utrecht en een plek nabij
+          Geldermalsen. Hier krijg je de kans om je medestudenten beter te leren kennen en de unieke sfeer van onze
+          vereniging te ervaren. Er wachten je diverse activiteiten, variërend van spellen tot sportuitdagingen, en het
+          geheel wordt afgesloten met een gezellig feest!
+        </p>
 
-      <p>
-        Om deel uit te maken van dit geweldige introductiekamp, dien je jezelf te registreren. Dit kan eenvoudigweg door
-        he volgende formulier in te vullen:
-      </p>
-      <a class="aanmelden" href="https://forms.gle/RGBfuZSz34ao8UgF7">Klik hier om je aan te melden voor introkamp</a>.
+        <p style="text-align: center">
+          Om deel uit te maken van dit geweldige introductiekamp, dien je jezelf hieronder te registreren.
+        </p>
+        <div class="buttons">
+          <a class="aanmelden" href="https://forms.gle/RGBfuZSz34ao8UgF7">Aanmelden voor introkamp</a>
+          <a class="wachtrij" href="https://forms.gle/qfFeLTfoCGJaE3ou9"> Inschrijvingen vol? Aanmelden wachtrij!</a>
+        </div>
 
-      <p>
-        Wees er snel bij want er zijn maar een beperkt aantal plekken!<br />
-        <a class="" href="https://forms.gle/qfFeLTfoCGJaE3ou9"> Inschrijvingen vol? Klik hier voor de wachtrij!</a>
-      </p>
+        <p style="text-align: center">Wees er snel bij want er zijn maar een beperkt aantal plekken!<br /></p>
 
-      <p>
-        Ons introductiekamp is de perfecte manier om helemaal op te gaan in de vereniging en nieuwe vriendschappen te
-        smeden. Een ervaring die je niet wilt missen! Dus meld je snel aan en we kijken ernaar uit je te zien bij het
-        introductiekamp!
-      </p>
+        <p>
+          Ons introductiekamp is de perfecte manier om helemaal op te gaan in de vereniging en nieuwe vriendschappen te
+          smeden. Een ervaring die je niet wilt missen! Dus meld je snel aan en we kijken ernaar uit je te zien bij het
+          introductiekamp!
+        </p>
+      </div>
 
-      <p>
-        Heb je nog vragen over het introductiekamp of iets anders gerelateerd aan de vereniging? Aarzel niet om ons te
-        bereiken via <a class="" href="mailto://introcom@indicium.hu"> introcom@indicium.hu</a>. We staan altijd klaar
-        om je te helpen.
-      </p>
+      <div>
+        <p>
+          Heb je nog vragen over het introductiekamp of iets anders gerelateerd aan de vereniging? Aarzel niet om ons te
+          bereiken via <a class="" href="mailto://introcom@indicium.hu"> introcom@indicium.hu</a>. We staan altijd klaar
+          om je te helpen.
+        </p>
+      </div>
     </section>
   </ContentContainer>
 </template>
 
 <style scoped lang="scss">
-img {
-  width: 80vw;
-  max-width: 400px;
-  margin: auto;
-  display: flex;
+em {
+  &.openict {
+    opacity: 0.84;
+    font-size: 0.8em;
+  }
+
+  &.datum {
+    font-style: normal;
+    background-image: linear-gradient(transparent 75%, var(--indi-green-2) 90%);
+    border-radius: 5px;
+  }
 }
 
-.aanmelden {
-  background-color: var(--indi-green-1);
-  padding: 8px;
-  margin: auto;
-  border-radius: 8px;
-  width: 300px;
-  justify-content: center;
+.intro {
   display: flex;
-  font-size: 1.5rem;
-  font-weight: 500;
-  color: var(--text-color);
-  text-align: center;
-  text-decoration: none;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+  padding: 0 1rem;
+
+  & > div {
+    background-color: var(--secondary-background-color);
+    border-radius: 8px;
+    width: 100%;
+    max-width: 1000px;
+    padding: 0.5rem 1.5rem;
+  }
+}
+
+img {
+  width: 80vw;
+  max-width: 600px;
+  margin: 10px auto;
+  display: flex;
+  border-radius: 5px;
+}
+
+.buttons {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 1rem;
+  padding: 1rem 0;
+
+  & > .aanmelden {
+    box-sizing: border-box;
+    background-color: var(--indi-green-1);
+    border: 2px solid var(--indi-green-1);
+    padding: 16px 8px;
+    border-radius: 8px;
+    max-width: 300px;
+    justify-content: center;
+    display: inline-block;
+    font-size: 1.5rem;
+    font-weight: 500;
+    color: var(--text-color);
+    text-align: center;
+    text-decoration: none;
+
+    &:hover {
+      background-color: var(--indi-green-2);
+    }
+  }
+
+  & > .wachtrij {
+    display: inline-block;
+    box-sizing: border-box;
+    background-color: transparent;
+    border: 2px solid var(--indi-blue-1);
+    padding: 16px 8px;
+    border-radius: 8px;
+    max-width: 300px;
+    justify-content: center;
+    font-size: 1.5rem;
+    color: var(--text-color);
+    text-align: center;
+    text-decoration: none;
+
+    &:hover {
+      background-color: var(--indi-blue-3);
+    }
+  }
+}
+
+h1 {
+  margin: 0.5rem 0;
 }
 </style>
