7da73f5f8424429c70647db015105d763f4649a8
GreenMan36 <Jaspervandenbremer@gmail.com>
2023-10-04 16:54:11 +0200

    UPDATE: Dispuut pagina

--- src/views/Dispuut.vue
+++ src/views/Dispuut.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import ContentContainer from '@/layouts/ContentContainer.vue';
 import disputen from '@/content/dispuut.json';
+/// <reference types="vite-svg-loader" />
 
 const [dispuut] = disputen;
 </script>
 
 <template>
   <ContentContainer>
-    <h1>Dispuut</h1>
+    <h1>Disputen</h1>
     <p id="intro"></p>
     <div class="dispuut" v-for="(dispuut, index) in disputen">
       <img alt="dispuutfoto" class="foto" :src="dispuut.imgUrl" />
@@ -25,7 +26,7 @@ const [dispuut] = disputen;
 
 .dispuut {
   border-bottom: 3px solid var(--indi-blue-1);
-  max-width: 1500px;
+  max-width: 1100px;
   margin: 0 auto;
   padding: 2em 0;
   display: grid;
@@ -35,6 +36,19 @@ const [dispuut] = disputen;
 
   @media screen and (max-width: #{$bp-tablet-lg}) {
     grid-auto-flow: row;
+  }
+
+  img {
+    height: 100%;
+    min-width: 320px;
+    border-radius: 10px;
+    transition: transform 0.2s;
+    background-image: linear-gradient(to bottom right, var(--indi-blue-1), var(--indi-blue-2));
+  }
+
+  img:hover {
+    transform: scale(1.1);
+    transition: transform 0.5s;
   }
 
   h2 {
@@ -48,6 +62,12 @@ const [dispuut] = disputen;
 
   &:last-child {
     border: none;
+  }
+
+  .dispuut-info {
+    border-radius: 10px;
+    padding: 1em 2em;
+    background-color: var(--secondary-background-color);
   }
 }
 </style>
