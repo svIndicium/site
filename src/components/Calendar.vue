0e10699b350fe2c3864ef4ccbc59dbb73be1b0f3
Siard <siard.ijpma@indicium.hu>
2024-01-02 21:42:55 +0100

    Add paragraph with text about no activities. Visible if visibleEvents is empty.

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -112,6 +112,10 @@ function extractHourAndMinutes(timeString: string) {
 <template>
   <h2 class="title">Agenda</h2>
   <div class="events-container">
+    <p v-if="!visibleEvents.length">
+      Binnenkort zijn er nog geen activiteiten.
+      Voeg de kalender toe aan je agenda om up-to-date te blijven!
+    </p>
     <div class="event" v-for="event in visibleEvents" :key="event.id">
       <div class="date">
         <span class="day">{{ event.start.toLocaleDateString('nl', { day: 'numeric' }) }}</span>
