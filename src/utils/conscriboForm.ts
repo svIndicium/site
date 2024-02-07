0d180b331e77174a064ba4149f00eef9665b5846
Siard <siard.ijpma@indicium.hu>
2024-02-07 19:50:39 +0100

    Refactor conscriboForm.ts variable names

--- src/utils/conscriboForm.ts
+++ src/utils/conscriboForm.ts
@@ -1,17 +1,16 @@
 export let fetchConscriboFormAndInjectFix = (src_url: string): void => {
-
   fetch(src_url)
     .then((response) => response.text())
-    .then((data) => {
+    .then((formScript) => {
 
-      data = data.replace(/proto\.toggleInvalidBic = function\(toggleValue\) \{([\s\S]*?)};/,
+      formScript = formScript.replace(/proto\.toggleInvalidBic = function\(toggleValue\) \{([\s\S]*?)};/,
         (match) => `${match}
   \tproto.toggleInvalidName = function(toggleValue) {
   \t\tthis._toggleInvalidOnElement(this.nameElement.ge(), toggleValue);
   \t};`
       );
 
-      data = data.replace(
+      formScript = formScript.replace(
         /if\(res\.bic\.length === 0\) \{([\s\S]*?)}/,
         (match) => `${match}\n
   \t\tif(res.name.length === 0) {
@@ -19,20 +18,20 @@ export let fetchConscriboFormAndInjectFix = (src_url: string): void => {
   \t\t}`
       );
 
-      data = data.replace(/obj\.toggleInvalidBic\(false\);/,
+      formScript = formScript.replace(/obj\.toggleInvalidBic\(false\);/,
         (match) => `${match}
    \t\t\tobj.toggleInvalidName(false)`
       );
 
-      data = data.replace(/obj\.toggleInvalidBic\(res\.bic\.length === 0\);/,
+      formScript = formScript.replace(/obj\.toggleInvalidBic\(res\.bic\.length === 0\);/,
         (match) => `${match}
   \t\t\tobj.toggleInvalidName(res.name.length === 0);`
       );
 
-      let scriptElement = document.createElement('script');
-      scriptElement.innerHTML = data;
+      let conscriboFormElement = document.createElement('script');
+      conscriboFormElement.innerHTML = formScript;
 
-      document.body.appendChild(scriptElement);
+      document.body.appendChild(conscriboFormElement);
     })
     .catch((error) => console.warn('Error occurred: ', error));
 }
