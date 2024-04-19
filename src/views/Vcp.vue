7c0460af7c41942ac1b203dc046ea22f5be5f326
GreenMan36 <Jaspervandenbremer@gmail.com>
2024-04-19 16:28:04 +0200

    UPDATE: VCP styling

--- src/views/Vcp.vue
+++ src/views/Vcp.vue
@@ -3,100 +3,110 @@ import ContentContainer from '@/layouts/ContentContainer.vue';
 import { currentVCP } from '@/content/vcp.json';
 </script>
 
+<!-- &#8203; is een zero-width-space, is nodig voor overflow op mobieltjes -->
 <template>
   <ContentContainer>
-    <h1>Vertrouwens Contact Personen</h1>
+    <h1>Vertrouwens&#8203;contact&#8203;personen</h1>
+    <p class="vcp-blerb">
+      Heb je vragen, problemen of wil je gewoon even praten? Neem dan contact op met een van onze
+      vertrouwenscontactpersonen. Zij zijn er voor jou!
+    </p>
+
     <div class="member" v-for="member in currentVCP.vcpMembers">
       <img
         class="member-photo"
         :src="`/assets/vcpphotos/${member.photo}`"
         :alt="member.name"
+        width="400"
+        height="500"
       />
       <div class="member-info">
         <h3>{{ member.name }}</h3>
         <h4>{{ member.status }}</h4>
         <p>{{ member.phonenumber }}</p>
-        <h4>{{ member.aboutme }}</h4>
-        <h2>Feitjes over mij</h2>
-        <p>{{ member.funfact1 }}</p>
-        <p>{{ member.funfact2 }}</p>
-        <p>{{ member.funfact3 }}</p>
+        <h4>Over mij</h4>
+        <p>{{ member.aboutme }}</p>
+        <h4>Feitjes over mij</h4>
+        <ul>
+          <li>{{ member.funfact1 }}</li>
+          <li>{{ member.funfact2 }}</li>
+          <li>{{ member.funfact3 }}</li>
+        </ul>
       </div>
     </div>
   </ContentContainer>
 </template>
 
 <style scoped lang="scss">
-  @import '@/assets/scss/variables.scss';
+@import '@/assets/scss/main.scss';
 
-  .content-container {
-    display: flex;
-    flex-direction: column;
+.content-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.vcp-blerb {
+  max-width: 800px;
+  margin: 0 auto;
+  text-align: center;
+}
+
+h1 {
+  text-align: center;
+  word-wrap: break-word;
+}
+
+.member {
+  display: flex;
+  gap: 3em;
+  justify-content: center;
+  flex-wrap: nowrap;
+  max-width: 1200px;
+  margin: 2em auto;
+
+  &:nth-child(even) {
+    flex-direction: row-reverse;
+  }
+}
+
+.member-info {
+  border-radius: 10px;
+  padding: 1em 2em;
+  background-color: var(--secondary-background-color);
+  p {
+    margin-bottom: 1em;
   }
 
-  h1 {
-    text-align: center;
+  h2 {
+    margin-top: 1em;
   }
 
-  .member {
-    display: flex;
-    gap: 3em;
-    justify-content: center;
-    flex-wrap: wrap;
-    max-width: 1170px;
-    margin: 2em auto;
-
-   
-    &:nth-child(even) {
-      flex-direction: row-reverse;
-    }
+  ul {
+    margin-top: 0.5em;
+    margin-left: 1em;
   }
+}
 
-  .member-info {
-    text-align: center;
-    p {
-      margin-bottom: 1em;
-    }
- 
-    h2 {
-      margin-top: 1em;
-    }
+.member-photo {
+  flex-shrink: 0;
+  border-radius: 10px;
+  transition: transform 0.2s;
+  object-fit: cover;
+  height: auto;
+  aspect-ratio: 4/5;
 
-  }
-
-  .member-photo {
-    width: 100%;
-    max-width: 400px;
-    border-radius: 10px;
+  &:hover {
+    transform: scale(1.05);
     transition: transform 0.2s;
-
-    &:hover {
-      transform: scale(1.05);
-      transition: transform 0.2s;
-    }
   }
+}
 
-  @media screen and (max-width: #{$bp-tablet-md}) {
-    .member {
-      gap: 1em;
-      margin: 2em auto;
-
-      &:nth-child(even) {
-        h3, h4 {
-          text-align: center;
-        }
-      }
-    }
-    h3, h4 {
-      text-align: center;
-    }
+@media (max-width: 1000px) {
+  .member {
+    flex-direction: column;
+    flex-wrap: wrap;
+    gap: 2em;
+    align-items: center;
   }
-
-  @media screen and (max-width: #{$bp-tablet-sm}) {
-    .member {
-      gap: 0;
-    }
-  }
-
-
+}
 </style>
