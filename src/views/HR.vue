9464e0d4f40de5006dda72df67eb4ca4217ffd88
GreenMan36 <Jaspervandenbremer@gmail.com>
2024-05-04 18:01:02 +0200

    UPDATE: document views are now a4 sized with a direct view button

--- src/views/HR.vue
+++ src/views/HR.vue
@@ -1,11 +1,37 @@
 <script setup lang="ts">
 import ContentContainer from '@/layouts/ContentContainer.vue';
 </script>
-
 <template>
   <ContentContainer>
-    <iframe src="/pdf/Huishoudelijk_Reglement_Studievereniging_Indicium.pdf" width="100%" height="800px" />
+    <div class="center-container">
+      <a class="button primary rounded indi-green-1" href="/pdf/Huishoudelijk_Reglement_Studievereniging_Indicium.pdf"
+        >Open in nieuw tablad</a
+      >
+
+      <iframe title="HR" src="/pdf/Huishoudelijk_Reglement_Studievereniging_Indicium.pdf" width="100%" height="800px" />
+    </div>
   </ContentContainer>
 </template>
 
-<style scoped></style>
+<style scoped>
+.center-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+
+  .button {
+    margin-top: 0.5rem;
+  }
+}
+
+iframe {
+  border: none;
+  /* make sure the iframe is a4 sized */
+  aspect-ratio: 210/297;
+  width: 100%;
+  max-width: 1000px;
+  height: auto;
+}
+</style>
--- src/views/Statuten.vue
+++ src/views/Statuten.vue
@@ -4,8 +4,33 @@ import ContentContainer from '@/layouts/ContentContainer.vue';
 
 <template>
   <ContentContainer>
-    <iframe src="/pdf/statutenindicium.pdf" width="100%" height="800px" />
+    <div class="center-container">
+      <a class="button primary rounded indi-green-1" href="/pdf/statutenindicium.pdf">Open in nieuw tablad</a>
+
+      <iframe title="Statuten" src="/pdf/statutenindicium.pdf" width="100%" height="800px" />
+    </div>
   </ContentContainer>
 </template>
 
-<style scoped></style>
+<style scoped>
+.center-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+
+  .button {
+    margin-top: 0.5rem;
+  }
+}
+
+iframe {
+  border: none;
+  /* make sure the iframe is a4 sized */
+  aspect-ratio: 210/297;
+  width: 100%;
+  max-width: 1000px;
+  height: auto;
+}
+</style>
