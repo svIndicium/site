4af12abcfbca5bff5f6bc1f1148d84945ad8ee37
Siard <siard.ijpma@indicium.hu>
2025-03-18 15:47:26 +0100

    Re-enable VCP components

--- src/views/Vcp.vue
+++ src/views/Vcp.vue
@@ -14,35 +14,35 @@ import Placeholder from '@/views/Placeholder.vue';
     </p>
 
 <!--    TODO: Remove when VCPs are updated-->
-    <Placeholder/>
+<!--    <Placeholder/>-->
 
 <!-- TODO: Uncomment when VCPs are updated -->
-<!--    <div class="member" v-for="member in currentVCP.vcpMembers">-->
-<!--      <img-->
-<!--        class="member-photo"-->
-<!--        :src="`/assets/vcpphotos/${member.photo}`"-->
-<!--        :alt="member.name"-->
-<!--        width="400"-->
-<!--        height="500"-->
-<!--      />-->
-<!--      <div class="member-entry">-->
-<!--        <div class="member-contact-info">-->
-<!--          <h3>{{ member.name }}</h3>-->
-<!--          <h4>{{ member.status }}</h4>-->
-<!--          <p>{{ member.phonenumber }}</p>-->
-<!--        </div>-->
-<!--        <div>-->
-<!--        <h4>Over mij</h4>-->
-<!--          <p>{{ member.aboutme }}</p>-->
-<!--          <h4>Feitjes over mij</h4>-->
-<!--          <ul>-->
-<!--            <li>{{ member.funfact1 }}</li>-->
-<!--            <li>{{ member.funfact2 }}</li>-->
-<!--            <li>{{ member.funfact3 }}</li>-->
-<!--          </ul>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </div>-->
+    <div class="member" v-for="member in currentVCP.vcpMembers">
+      <img
+        class="member-photo"
+        :src="`/assets/vcpphotos/${member.photo}`"
+        :alt="member.name"
+        width="400"
+        height="500"
+      />
+      <div class="member-entry">
+        <div class="member-contact-info">
+          <h3>{{ member.name }}</h3>
+          <h4>{{ member.status }}</h4>
+          <p>{{ member.phonenumber }}</p>
+        </div>
+        <div>
+        <h4>Over mij</h4>
+          <p>{{ member.aboutme }}</p>
+          <h4>Feitjes over mij</h4>
+          <ul>
+            <li>{{ member.funfact1 }}</li>
+            <li>{{ member.funfact2 }}</li>
+            <li>{{ member.funfact3 }}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </ContentContainer>
 </template>
 
